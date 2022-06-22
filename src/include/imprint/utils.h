6142e0cced25f08f1812ad1d47b2efbfd072721c
Peter Bjorklund <piot@hotmail.com>
2022-06-22 17:38:38 +0200

    chore: add headers

--- src/include/imprint/utils.h
+++ src/include/imprint/utils.h
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Peter Bjorklund. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 #ifndef IMPRINT_UTILS_H
 #define IMPRINT_UTILS_H
 
